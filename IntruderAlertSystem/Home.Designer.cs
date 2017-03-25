921e007068fb636b57ddca2946febe110ae821b5
Siddharth Varjivan <varjivan.siddharth@gmail.com>
2017-03-25 02:43:51 +0000

    Cleaned up

--- IntruderAlertSystem/Home.Designer.cs
+++ IntruderAlertSystem/Home.Designer.cs
@@ -32,7 +32,7 @@
             this.Y = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.crtFloor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // X
@@ -67,22 +67,22 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Y";
             // 
-            // button1
+            // crtFloor
             // 
-            this.button1.Location = new System.Drawing.Point(407, 64);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Create Floor";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.crtFloor.Location = new System.Drawing.Point(407, 64);
+            this.crtFloor.Name = "crtFloor";
+            this.crtFloor.Size = new System.Drawing.Size(100, 23);
+            this.crtFloor.TabIndex = 4;
+            this.crtFloor.Text = "Create Floor";
+            this.crtFloor.UseVisualStyleBackColor = true;
+            this.crtFloor.Click += new System.EventHandler(this.crtFloor_Click);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(519, 311);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.crtFloor);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Y);
@@ -100,6 +100,6 @@
         private System.Windows.Forms.TextBox X;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button crtFloor;
     }
 }
--- IntruderAlertSystem/Home.cs
+++ IntruderAlertSystem/Home.cs
@@ -13,6 +13,7 @@ namespace Alarm
     public partial class Home : Form
     {
         private static Home home = null;
+        private DataGridView test;
 
         public Home()
         {
@@ -29,56 +30,26 @@ namespace Alarm
             return home;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void crtFloor_Click(object sender, EventArgs e)
         {
-            // https://msdn.microsoft.com/en-us/library/aa984255(v=vs.71).aspx
-            DataGridView test = new DataGridView();
-            test.DataSource = null;
 
-            // Remove headings because we only want cells,
-            // which makes the table look more like a floor plan.
-            test.RowHeadersVisible = false;
-            test.ColumnHeadersVisible = false;
+            test = CStuff.creatDG(Convert.ToInt32(X.Text), Convert.ToInt32(Y.Text));
+          
+             Y.Visible = false;
+             X.Visible = false;
+             crtFloor.Visible = false;
+             label1.Visible = false;
+             label2.Visible = false;
 
-            // setup number of rooms
-            test.ColumnCount = Convert.ToInt32(X.Text);
-            test.RowCount = Convert.ToInt32(Y.Text);
-
-            // set start location to top-left corner
+            // // set start location to top-left corner
             const int START_LOCATION = 25;
-            test.Location = new Point(START_LOCATION, START_LOCATION);
 
-            // set cell lengths to be square and decently sized, so it represents a "room"
-            const int CELL_LENGTH = 100;
-
-            foreach (DataGridViewColumn column in test.Columns)
-            {
-                column.Width = CELL_LENGTH;
-            }
-
-            foreach (DataGridViewRow row in test.Rows)
-            {
-                row.Height = CELL_LENGTH;
-            }
-
-            // set the size of the table to be the perfect fit (no scroll bars / blank space)
-            // http://stackoverflow.com/questions/7412098/fit-datagridview-size-to-rows-and-columnss-total-size
-            // http://stackoverflow.com/questions/6651487/programmatically-resize-datagridview-to-remove-scroll-bars
-            int borderOffset = (test.BorderStyle == BorderStyle.None) ? 0 : 2;
-
-            int newHeight = test.Rows.GetRowsHeight(DataGridViewElementStates.Visible) + borderOffset;
-            int newWidth = test.Columns.GetColumnsWidth(DataGridViewElementStates.Visible) + borderOffset;
-
-            // set the adjusted size and remove the redundant scroll bars
-            test.ClientSize = new Size(newHeight, newWidth);
-            test.ScrollBars = ScrollBars.None;
-
-            // add table to form
+            // // add table to form
             getInstance().Controls.Add(test);
 
             // set the form size to be the table with some padding
-            int padding = START_LOCATION * 2;
-            getInstance().ClientSize = new Size(test.Height + padding, test.Width + padding);
+           int padding = START_LOCATION * 2;
+           getInstance().ClientSize = new Size(test.Height + padding, test.Width + padding);
         }
     }
 }
